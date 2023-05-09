17a811843a02b23f269f67c3367c1332d95039ba
IbukunOgwu <ijogwu@yahoo.com>
2023-05-09 16:00:25 -0700

    Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -50,6 +50,6 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
             print("")
             return
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print(f"{}: {}".format(k, v)) for k, v in instances]
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
